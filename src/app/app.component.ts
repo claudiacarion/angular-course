0f7a6545cc14bf7b23fa4fe5320bd9b3d0f866bb
claudiacarion <claudia.carion@gmail.com>
2023-03-15 15:43:28 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-course';
+  data = {
+    title: 'Learning Angular'
+  };
+
+  changeTitle() {
+    this.data.title = 'lol jk';
+  };
+
+  onKeyUp(newTitle: string) {
+    this.data.title = newTitle;
+  };
 }
