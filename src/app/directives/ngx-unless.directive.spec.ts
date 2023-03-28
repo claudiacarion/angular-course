9b52a469c4d99bb02a997da32f8812a39fbfd137
claudiacarion <claudia.carion@gmail.com>
2023-03-28 16:14:22 +0200

    fetching data

--- src/app/directives/ngx-unless.directive.spec.ts
+++ src/app/directives/ngx-unless.directive.spec.ts
@@ -1,8 +1,8 @@
-import { NgxUnlessDirective } from './ngx-unless.directive';
+// import { NgxUnlessDirective } from './ngx-unless.directive';
 
-describe('NgxUnlessDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NgxUnlessDirective();
-    expect(directive).toBeTruthy();
-  });
-});
+// describe('NgxUnlessDirective', () => {
+//   it('should create an instance', () => {
+//     const directive = new NgxUnlessDirective();
+//     expect(directive).toBeTruthy();
+//   });
+// });
---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Course } from "../model/course";
+
+@Injectable({
+  providedIn: "root"
+})
+
+export class CoursesService {
+  
+    constructor(private http: HttpClient) {
+  
+  }
+
+  loadCourses(): Observable<Course[]>{
+
+    const params = new HttpParams()
+      .set("page", "1")
+      .set("pageSize", "10");
+
+    return this.http.get<Course[]>('/api/courses', {params})
+
+  }
+
+}
