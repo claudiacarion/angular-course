2859464c70e2fbfb208d7428f0641d69dd309a8f
claudiacarion <claudia.carion@gmail.com>
2023-03-28 11:41:05 +0200

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,16 @@ export class AppComponent {
 
   courses = COURSES;
 
+  title = COURSES[0].description;
+
+  price = 9.9753111;
+
+  startDate = new Date(2000, 0, 1);
+
+  rate = 0.67;
+
+  course = COURSES[0];
+
   onCourseSelected(course:Course) {
     console.log("Card clicked", course);
   }
